e182e8a2de9d70fde175e40007596df8e968bc60
Lina <wulinthu@gmail.com>
2026-01-07 17:33:15 +0000

    Section 2 Angular Essentials - Adding tasks component and displaying username when selected as exercise

---
+++ attachments/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
@@ -0,0 +1,13 @@
+import { UserComponent } from './../user/user.component';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+  @Input({required: true}) name!: string;
+}
