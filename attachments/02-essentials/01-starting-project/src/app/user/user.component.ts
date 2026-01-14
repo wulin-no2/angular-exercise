7585707f76546ec9c027ddedb0e8971c5ae4aaa4
Lina <wulinthu@gmail.com>
2026-01-14 19:35:54 +0000

    Section2-Adding shared wrapper app-card

--- attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,11 +1,12 @@
 import { Component ,signal, computed,input, output, Input, Output, EventEmitter} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
