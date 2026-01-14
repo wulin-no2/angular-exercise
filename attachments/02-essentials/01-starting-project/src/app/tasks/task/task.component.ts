1896007af9415ba225bb4135564d758555ad94ed
Lina <wulinthu@gmail.com>
2026-01-14 19:40:34 +0000

    Section2-Adding date pipe to due date

--- attachments/02-essentials/01-starting-project/src/app/tasks/task/task.component.ts
+++ attachments/02-essentials/01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,20 +1,20 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from './task.model';
-import { CardComponent } from "../../shared/card/card.component";
+import { CardComponent } from '../../shared/card/card.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
 })
 export class TaskComponent {
-  @Input({required: true}) task!: Task;
+  @Input({ required: true }) task!: Task;
   @Output() complete = new EventEmitter<string>();
 
   onCompleteTask() {
     this.complete.emit(this.task.id);
   }
-
 }
